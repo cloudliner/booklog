ba019fc8751896e57321d12a46d0001b2934d08b
Tomomi Kawabe <girrrlande@gmail.com>
2016-09-24 07:27:29 +0000

    Update controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,12 @@
 class WelcomeController < ApplicationController
+
+  def result
+    RakutenWebService.configuration do |c|
+      c.application_id = ENV["APPID"]
+      c.affiliate_id = ENV["AFID"]
+  end
+
+      @books = RakutenWebService::Books::Book.search(:author => "夏目漱石",)
+      erb :books_result
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  post "welcome/result" => "welcome#result"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
